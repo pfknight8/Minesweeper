7ba79a52bb252f2d2c9aa603713077e385e596c3
Patrick Knight <pfknight2@gmail.com>
2022-07-02 16:51:04 -0400

    JS functions to create grid, populate bombs, and count in nearby tiles

--- script.js
+++ script.js
@@ -4,13 +4,109 @@
 // Global Variables //
 //////////////////////
 
-//starh
+//start
 
 //////////////////////
 // Functions        //
 //////////////////////
 
-//start
+//Creates a grid matrix, such as [[j0, j1, j2,], [j0, j1, j2], [j0, j1, j2]] where each set of 'j' is an an index of 'i'.
+const makeGrid = (numRows, numCols) => {
+  //create a grid stored as a matrix (array of arrays).
+  let gridMatix = [];
+  for (let i = 0; i < numRows; i++) {
+    let innerArray = [];
+    for (let j = 0; j < numCols; j++) {
+      innerArray[j] = null;
+    }
+    gridMatix[i] = innerArray;
+  }
+  return gridMatix;
+}
+
+//Takes an array and places a set number of bombs within them.
+const populateGrid = (gameGrid, numBombs) => {
+  while (numBombs > 0) {
+    let i = Math.floor(Math.random()*gameGrid.length);
+    let j = Math.floor(Math.random()*gameGrid[0].length); //assumes all inner arrays have same length.
+    if (gameGrid[i][j] === null) {
+      gameGrid[i][j] = 'bomb';
+      numBombs--;
+    }
+  }
+  return gameGrid;
+}
+
+//Evaluate the array to place numbers.
+const lookNearby = (gameGrid) => {
+  let iMin = 0;
+  let jMin = 0;
+  let iMax = gameGrid.length - 1;
+  let jMax = gameGrid[0].length - 1;
+  let bombCount = 0;
+  for (let i=0; i<=iMax; i++) {
+    for (let j=0; j<=jMax; j++) {
+      if (gameGrid[i][j] === 'bomb') {
+        //Skip
+      } else if (i>0 && i<iMax) {
+        for (let rowIn=i-1; rowIn<=i+1; rowIn++) {
+          for (let colIn=j-1; colIn<=j+1; colIn++) {
+            if (colIn < 0) {
+              //Skip
+            } else if (colIn > jMax) {
+              //Skip
+            } else {
+              if(gameGrid[rowIn][colIn] === 'bomb') {
+                bombCount+=1;
+              }
+            }
+          }
+        }
+        gameGrid[i][j] = bombCount;
+        bombCount = 0;
+      } else if (i == 0) {
+        for (let rowIn=i; rowIn<=i+1; rowIn++) {
+          for (let colIn=j-1; colIn<=j+1; colIn++) {
+            if (colIn < 0) {
+              //Skip
+            } else if (colIn > jMax) {
+              //Skip
+            } else {
+              if(gameGrid[rowIn][colIn] === 'bomb') {
+                bombCount+=1;
+              }
+            }
+          }
+        }
+        gameGrid[i][j] = bombCount;
+        bombCount = 0;
+      } else if (i == iMax) {
+        for (let rowIn=i-1; rowIn<=i; rowIn++) {
+          for (let colIn=j-1; colIn<=j+1; colIn++) {
+            if (colIn < 0) {
+              //Skip
+            } else if (colIn > jMax) {
+              //Skip
+            } else {
+              if(gameGrid[rowIn][colIn] === 'bomb') {
+                bombCount+=1;
+              }
+            }
+          }
+        }
+        gameGrid[i][j] = bombCount;
+        bombCount = 0;
+      }
+    }
+  }
+  return gameGrid;
+}
+
+let gameGrid = makeGrid(8, 8);
+console.log(gameGrid);
+console.log(gameGrid[2].length);
+let gameGridMined = lookNearby(populateGrid(gameGrid, 8));
+console.log(gameGridMined);
 
 //////////////////////
 // Event Listeners  //
